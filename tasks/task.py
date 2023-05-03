385b61abde83274d6b88f615742c28966aae0341
Gruzin <gruzd.roman@inbox.ru>
2023-05-03 15:22:46 +0300

    Commit

--- tasks/task.py
+++ tasks/task.py
@@ -2,5 +2,15 @@ import os
 
 
 class Cd:
-    pass
-    # TODO: please add your code here
+    def __init__(self, dir_path):
+        if not os.path.isdir(dir_path) or not os.path.exists(dir_path):
+            raise ValueError(f"{dir_path} doesn't exist")
+        self.dir_path = dir_path
+
+    def __enter__(self):
+        self.last_dir = os.getcwd()
+        os.chdir(self.dir_path)
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        os.chdir(self.last_dir)
